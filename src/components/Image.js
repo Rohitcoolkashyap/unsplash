4a3f947bd23aee13c96a064bc2b8108c63c8dbf7
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-09-10 13:17:15 +0530

    third

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,49 +3,49 @@ import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
 import UnsplashImage from "./UnsplashImage";
 export default function Image() {
-     const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
 
-     useEffect(() => {
-       fetchImages();
-     }, []);
+  useEffect(() => {
+    fetchImages();
+  }, []);
 
-     const fetchImages = async (count = 10) => {
-       const apiRoot = "https://api.unsplash.com/collections/1580860/photos";
+  const fetchImages = async (count = 10) => {
+    const apiRoot = "https://api.unsplash.com/collections/1580860/photos";
 
-       const accessKey = process.env.ACCESS_KEY;
+    const accessKey = process.env.ACCESS_KEY;
 
-       await axios
-         .get(`${apiRoot}/?client_id=${accessKey}&count=${count}`)
-         .then((res) => {
-           setImages([...images, ...res.data]);
-         })
-         .catch((e) => console.log(e));
-     };
+    await axios
+      .get(`${apiRoot}/?client_id=${accessKey}&count=${count}`)
+      .then((res) => {
+        setImages([...images, ...res.data]);
+      })
+      .catch((e) => console.log(e));
+  };
 
-    return (
-      <div className="image1"> 
-        {images.length !== 0 ? (
-          <div className="photos">
-            <InfiniteScroll
-              dataLength={images.length}
-              next={fetchImages}
-              hasMore={true}
-              loader={
-                <div>
-                  <img className="loading1" src="Rolling-1s-200px.svg" alt="" />
-                </div>
-              }
-            >
-              {images.map((image) => (
-                <UnsplashImage url={image.urls.thumb} key={image.id} />
-              ))}
-            </InfiniteScroll>
-          </div>
-        ) : (
-          <div>
-            <img className="loading1" src="Rolling-1s-200px.svg" alt="" />
-          </div>
-        )}
-      </div>
-    );
+  return (
+    <div className="image1">
+      {images.length !== 0 ? (
+        <div className="photos">
+          <InfiniteScroll
+            dataLength={images.length}
+            next={fetchImages}
+            hasMore={true}
+            loader={
+              <div>
+                <img className="loading1" src="Rolling-1s-200px.svg" alt="" />
+              </div>
+            }
+          >
+            {images.map((image) => (
+              <UnsplashImage url={image.urls.thumb} key={image.id} />
+            ))}
+          </InfiniteScroll>
+        </div>
+      ) : (
+        <div>
+          <img className="loading1" src="Rolling-1s-200px.svg" alt="" />
+        </div>
+      )}
+    </div>
+  );
 }
